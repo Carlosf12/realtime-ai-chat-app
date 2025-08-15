c0c602baa3d7066d0e9730b538b233c1a904668a
Carlosf12 <carlosfigueira1212@gmail.com>
2025-08-15 00:20:18 -0400

    feat: Set up user module, WebSocket gateway, and fixed all dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+
 
 @Module({
   imports: [
@@ -11,6 +14,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
+      
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('POSTGRES_HOST'),
@@ -18,11 +22,12 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         username: configService.get<string>('POSTGRES_USER'),
         password: configService.get<string>('POSTGRES_PASSWORD'),
         database: configService.get<string>('POSTGRES_DB'),
-        entities: [], // We'll add our entities here later
+        entities: [User], // We'll add our entities here later
         synchronize: true, // Auto-create tables (for development only)
       }),
       inject: [ConfigService],
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
